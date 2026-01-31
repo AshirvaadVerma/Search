ee7257374cdb78f307d774040e24fd1e7ae71b45
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2026-01-31 12:22:54 +0530

    Replace email questions with trivia questions

--- script.js
+++ script.js
@@ -1,4 +1,208 @@
-let questions=["aaradhya6553@gmail.com", "abhi828886@gmail.com", "abhishek29296@gmail.com", "advika292008@gmail.com", "akshat17112006@gmail.com", "dev003135@gmail.com", "devansh02030@gmail.com", "farhan97286@gmail.com", "farhanverma07@gmail.com", "gabbar727818@gmail.com", "gargi68757@gmail.com", "garuda82829@gmail.com", "harshpatel92828@gmail.com", "raju755785@gmail.com", "sanjay857988@gmail.com", "saumya67488@gmail.com", "shreya82928@gmail.com", "shubham86545@gmail.com", "sooraj82892@gmail.com", "sureshpatel71817@gmail.com", "vijay8575337@gmail.com", "watson82827@gmail.com", "aditya010328@gmail.com", "aashi1382000@gmail.com", "dhruv46674@gmail.com", "shishir85842@gmail.com", "suyash55573@gmail.com"];
+let questions=["firebase",
+               "tin element symbol",
+               "lava agni 4 8+128",
+               "lava agni 3 8+128",
+               "when will lava launch android 16 for lava agni 4?",
+               "lava play ultra 8+128",
+               "dell 17 plus laptop",
+               "dell",
+               "boat valour ring 2",
+               "revolt electric bike",
+               "mahindra 7 seater electric car",
+               "tata 7 seater electric car",
+               "zoho pay logo",
+               "when will zoho pay launch",
+               "phi symbol",
+               "Which country is known as “the Land of festivals”,
+               "Where is Himalayan Mountaineering Institute located?",
+  "For how many disciplines is Nobel Prize awarded?",
+  "Name the tennis Player who is known as “The King of Clay”?",
+  "Which Indian City has been declared as World Heritage City (WHC) by UNESCO recently?",
+  "Who wrote the famous novel “James pond”?",
+  "Who won Oscar award for 36 times?",
+  "Who established the first Blood Bank?",
+  "Who is the Indian to beat the computers in Mathematical Wizardry?",
+               "Which Country Hosted 2017 World Petroleum Congress (WPC)",
+  "Who wrote the book, “India at risk”?",
+  "Where is the International court of Justice located?",
+  "What is Limnology?",
+  "Which City in India is known as the “City of Joy”?",
+  "Who was the first Asian winner of the Nobel Prize",
+               "When was Tooth brush invented?
+  Which pair of Cities in India is known as the “Twin Cities“?
+  Which City in India is known as the “City of Golden Gate”?
+  What is Palaeontology?
+  Who is known as “A Lady with the Lamp”?
+  Thomas cup is associated with which sport?
+  What is India’s rank in 2017 Global Cyber security Index?
+  Which is the Busiest Bridge in India?
+  Which City is known as the “City of Seven Hills”?
+  Who was the first woman chief minister of India?
+  Which Country is the leading producer of Diamond?
+  Which Vitamin is responsible for the Clotting of Blood?
+  Who invented Dynamite?
+  Where is the headquarters of United Nations Organisation (UNO) located?
+  Who invented Richter scale?
+  Who is known as “Indian Bismarck”?
+  What is the Expansion of DRDL",
+               "What is Eugenics?
+  Which Country gifted “the statue of Liberty” to The America?
+  Which City is known as the “Queen of Arabian Sea”?
+  Which is the Biggest Library in the world?
+  Who was the first woman recipient of Magsaysay award?
+  Which country is known as “the Land of Thousand lakes”?
+  Which Country is the leading producer of Tea?
+  Who was the first woman IAS Officer of India?
+  Who wrote the book, “The God of Small Things”?
+  Expand IGNOU.
+  Who was the first Woman President of Un General Assembly?
+  Which City in India is known as the “City of Dreams”?
+  Which is the Largest Temple in the world?
+  Expand OMR.
+  Which instrument is used to measure the Blood Pressure?
+  Who wrote the book, “Paradise lost”",
+               "What is Eugenics?
+  Which Country gifted “the statue of Liberty” to The America?
+  Which City is known as the “Queen of Arabian Sea”?
+  Which is the Biggest Library in the world?
+  Who was the first woman recipient of Magsaysay award?
+  Which country is known as “the Land of Thousand lakes”?
+  Which Country is the leading producer of Tea?
+  Who was the first woman IAS Officer of India?
+  Who wrote the book, “The God of Small Things”?
+  Expand IGNOU.
+  Who was the first Woman President of Un General Assembly?
+  Which City in India is known as the “City of Dreams”?
+  Which is the Largest Temple in the world?
+  Expand OMR.
+  Which instrument is used to measure the Blood Pressure?
+  Who wrote the book, “Paradise lost”",
+               "Which was the first Country to conduct Civil Services Competitions?
+  Which is the Tallest Tower in the world?
+  What is the name of Japan Parliament?
+  Which is the Highest Award given for Lifetime Achievement in Sports and games in India?
+  Name the central minister who launched GST Rates Finder App.
+  Name the Indian King Who was Known as “Napoleon of India”.
+  Which country is known as “the Land of Morning Calm”?
+  Which is the National aquatic Animal of India?
+  Which is the World’s Busiest Airport?
+  Where is the Ghana Bird Sanctuary located?
+  Who coined the term “Green Revolution”?
+  Which is the first Country to make a Constitution?
+  What is the ratio of width of our National flag to its Length?
+  Where is the headquarters of Sahitya Academy Located?
+  Which is the Longest River in India?
+  ‘Madhubani’, a Style of folk Paintings, is popular in which state of India?",
+               "How Many Countries share their Borderline with India?
+  Rabindranath Tagore’s “Jana Gana Mana” was adopted as India’s
+National Anthem. How many stanzas of the song were adopted?
+  Who composed the National song of India “Vande Matram”?
+  Who is the present Chief election Commissioner of India?
+  Who Founded Nalanda University?
+  What is Biogeography?
+  Which is the Junction Point of Eastern and Western Ghats?
+  Which City is known as the “Forbidden City”?
+  Who was the first woman to receive Bharat Ratna?
+  Which is the Longest Dam in India?
+  Who is known as “the Man of Destiny”?
+  When is the World Tourism Day Observed?
+  Which City is known as the “Chocolate City”?
+  Who wrote the book, “The Road Ahead”?
+  Who is the present Central Minister of Women Welfare and Child Development?
+  What is Exobiology?",
+               "Expand GPRS.
+  Who is the present Central Minister of Youth Affairs and Sports?
+  Who wrote the Tamil Novel, “Sila Nerangalil Sila Manidhargal”?
+  What is the expansion of WMO?
+  Which country is known as “the Land of Maple”?
+  When is the International Youth’s Day Celebrated?
+  Which is the name of the Award given for Excellence in Space Research in India?
+  Who was the first Indian to receive Nobel Prize in Economics?
+  Where is the Kaziranga Park Located?
+  Which is the Highest Gallantry Award in India?
+  When is National Statistics Day Celebrated?
+  What is the expansion of USB?
+  Who was the first Indian to become the member of the British Parliament?
+  What is the Total Duration of Foot ball Match?
+  Who wrote the book, “Discovery of India”?
+  In Which Year, was the Pulitzer Prize Introduced?",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "",
+               "shreya82928@gmail.com", "shubham86545@gmail.com", "sooraj82892@gmail.com", "sureshpatel71817@gmail.com", "vijay8575337@gmail.com", "watson82827@gmail.com", "aditya010328@gmail.com", "aashi1382000@gmail.com", "dhruv46674@gmail.com", "shishir85842@gmail.com", "suyash55573@gmail.com"];
 let pos;
 document.addEventListener("DOMContentLoaded", function()
                           {
@@ -25,3 +229,4 @@ document.getElementById('copy').addEventListener('click', function()
                                                      document.getElementById('question').value=questions[pos];
                                                      document.getElementById('copy').textContent="Copy " + (pos+1);
                                                  });
+
